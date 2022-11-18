d02c697852ea7a589c04a9b93b8854b85c02754d
thomas.rogister <thomasrogister@protonmail.com>
2022-11-18 13:33:24 +0100

    debug key list - tags & rating

--- src/components/tags/tags.jsx
+++ src/components/tags/tags.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "../../style/style.css";
 
 function Tags({ tags}) {
-  return tags.map((tag, index) => (
-    <div className="tag__container" key={`${tag}-${index}`}>
+  return tags.map((tag, i) => (
+    <div className="tag__container" key={`${tag}-${i}`}>
       <span className="tag__content">{tag}</span>
     </div>
   ));
